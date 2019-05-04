a1940ec62f5e7e763c59044f5ca355c24f6eb716
Lisa Modenbach <lmodenba@mail.ccsf.edu>
2019-05-04 15:46:17 -0700

    Added blurbs directory under docs and personal blurb text #9, added fakeProject.sh under utils

---
+++ utils/fakeProject.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "\n\nhello, world\n\n"
+echo -e "Here is a list of beers for you to use:\n"
+echo -e "Bud Light\nBudweiser\nCorona\nCoors\nModelo\nNatural Light\nRolling Rock"
+echo -e "Old English\nSt. Ides\nMichelob\nMiller\nBusch\nKeystone\nO'Doul's\nKing Cobra"
+echo -e "Colt 45\nShock Top\nBeck's\nTecate\nPabst Blue Ribbon\n\n"
+echo -e "Have a great headache!\n\n"
